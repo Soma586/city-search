1f5ae05cc5a1805357e1c20ce060d45baae2d412
Tarik <lightspeedkey@yahoo.com>
2019-06-12 02:07:22 -0400

    Updates

---
+++ precache-manifest.161b40bee5823cb15f807444c215653c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6673b19c01f4170d3ed5a1839d42ec",
+    "url": "/city-search/./index.html"
+  },
+  {
+    "revision": "06faf3d4992f42b524a2",
+    "url": "/city-search/./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0a6a3b9022cbb4b407e6",
+    "url": "/city-search/./static/js/2.8fd7973f.chunk.js"
+  },
+  {
+    "revision": "06faf3d4992f42b524a2",
+    "url": "/city-search/./static/js/main.3e576964.chunk.js"
+  },
+  {
+    "revision": "f2aa045734bf93710313",
+    "url": "/city-search/./static/js/runtime~main.57395e25.js"
+  }
+]);
